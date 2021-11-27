665887aab4d7d7d54f1659ee30efcdaf27fec23a
raythompsonwebdev <ray_thomp@hushmail.com>
2021-11-27 02:15:42 +0000

    added comments

--- index.test.js
+++ index.test.js
@@ -4,13 +4,19 @@ test("input text added to list container as list item", () => {
     itemToAdd.value = "test"; // step 2
     const submitButton = document.querySelector("input[type='submit']");
     submitButton.click(); // step 3   
+    
+    //get Ul element
     const todoList = document.getElementById("todoList");
+    
+    //get all existing li elements 
     let children = document.getElementsByTagName("Li");
 
+    // loop over collection with existing LI elements.
     for (let i = 0; i < children.length; i++) {
+        //check if UL contains an LI element
         if(todoList.contains(children[i])){
-            equal(!children[i], false, "LI element created"); // step 4
-            
+            // compare LI element with first LI element in collection if it exists, if both are true element has been created.
+            equal(children[i], children[0], "LI element created"); // step 4            
         }
 
         if(children[i].textContent === itemToAdd.value){
