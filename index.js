25b75710dbea3f3ceeefe9dc46a7ac926611d0d2
miahbates <miahbates@icloud.com>
2021-11-28 19:47:07 +0000

    add checkbox to elements added to list

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ submitButton.addEventListener("click", function (e){
     const todoList = document.getElementById("todoList");      
          
     const itemContainer = document.createElement("LI");
-    itemContainer.innerHTML = itemToAdd;
+     
+    // create  input element
+    const checkbox = document.createElement("input")
+    // assign type to checkbox
+    checkbox.setAttribute("type", "checkbox");
+    // create span element to wrap li and checkbox inside 
+    const span = document.createElement("span")
+    // add input item to span
+    span.innerHTML = itemToAdd;
+
+    // append span and checkbox to Li element
+    itemContainer.appendChild(checkbox);
+    itemContainer.appendChild(span);
     todoList.appendChild(itemContainer); 
 });
 
