ce13347f903b4745e3a58779c54e046d7fff1fbb
miahbates <miahbates@icloud.com>
2021-12-04 13:20:51 +0000

    inital commit for miah's branch - deleted old code

--- index.js
+++ index.js
@@ -5,121 +5,123 @@ const todoList = document.getElementById("todoList");
 let idx = 0;
 
 const addItemToList = (e) => {
-	e.preventDefault();
+  e.preventDefault();
+ 	// test value
+  let itemToAdd = document.getElementById("addItem").value;
 
-	// test value
-	let itemToAdd = document.getElementById("addItem").value;
-	//const todoList = document.getElementById("todoList");
+ 	// LI element
+ 	const itemContainer = document.createElement("LI");
+ 	itemContainer.setAttribute("class", `list-item`);
 
-	//LI element
-	const itemContainer = document.createElement("LI");
-	itemContainer.setAttribute("class", `list-item`);
-	//label
-	const itemLabel = document.createElement("LABEL");
-	// checkbox
-	const checkBox = document.createElement("INPUT");
+ 	// label
+ 	const itemLabel = document.createElement("LABEL");
 
-	// delete button
-	const deleteButton = document.createElement("BUTTON");
-	deleteButton.type = "submit";
-	deleteButton.setAttribute("id", "deleteButton");
-	deleteButton.textContent = "X";
+ 	// checkbox
+ 	const checkBox = document.createElement("INPUT");
 
-	// label
-	//id number appended to for attribute value of label so that it matches id value of checkbox
-	itemLabel.setAttribute("for", `listitem-${idx}`);
+ 	// delete button
+ 	const deleteButton = document.createElement("BUTTON");
+ 	deleteButton.type = "submit";
+ 	deleteButton.setAttribute("id", "deleteButton");
+ 	deleteButton.textContent = "X";
 
-	// add input text to label
-	itemLabel.innerHTML = itemToAdd;
+ 	// label
+ 	// id number appended to for attribute value of label so that it matches id value of checkbox
+ 	itemLabel.setAttribute("for", `listitem-${idx}`);
 
-	// set input type to checkbox
-	checkBox.type = "checkbox";
+ 	// add input text to label
+ 	itemLabel.innerHTML = itemToAdd;
+ 
+ 	// set input type to checkbox
+ 	checkBox.type = "checkbox";
 
-	//id number appended to id value of checkbox so that it matches for attribute value in label
-	checkBox.setAttribute("id", `listitem-${idx}`);
+ 	//id number appended to id value of checkbox so that it matches for attribute value in label
+ 	checkBox.setAttribute("id", `listitem-${idx}`);
 
-	// give checkbox a class for later retrieval
-	checkBox.setAttribute("class", "list_item");
+ 	// give checkbox a class for later retrieval
+ 	checkBox.setAttribute("class", "list_item");
 
-	itemContainer.appendChild(itemLabel);
-	itemContainer.appendChild(checkBox);
-	itemContainer.appendChild(deleteButton);
-	todoList.appendChild(itemContainer);
+ 	itemContainer.appendChild(itemLabel);
+ 	itemContainer.appendChild(checkBox);
+ 	itemContainer.appendChild(deleteButton);
+ 	todoList.appendChild(itemContainer);
 
-	// increment id for every list item created
-	idx++;
+ 	// increment id for every list item created
+ 	idx++;
 
-	// set searchbar back to empty
+ 	// set searchbar back to empty
   document.getElementById("addItem").value = "";
 
-	// invoke completed task function
-	completedTasks();
+ 	// invoke completed task function
+ 	completedTasks();
 };
 
 const completedTasks = () => {
-	// complete tasks
-	let completeTask = todoList.children;
+ 	// complete tasks
+ 	let completeTask = todoList.children;
 
-	if (completeTask.length === 0) {
-		console.log("no items");
-	}
+ 	if (completeTask.length === 0) {
+ 		console.log("no items");
+ 	}
 
-	for (let i = 0; i < completeTask.length; i++) {
-		// get label
-		let completeLabel = completeTask[i].childNodes[0];
+ 	for (let i = 0; i < completeTask.length; i++) {
+ 		// get label
+ 		let completeLabel = completeTask[i].childNodes[0];
 
-		//get checkbox
-		let completeCheckBox = completeTask[i].childNodes[1];
+ 		//get checkbox
+ 		let completeCheckBox = completeTask[i].childNodes[1];
 
-		completeLabel.addEventListener("click", function (e) {
-			console.log("index : label clicked");
-		});
+ 		completeLabel.addEventListener("click", function (e) {
+ 			console.log("index : label clicked");
+ 		});
 
-		completeCheckBox.addEventListener("change", (e) => {
-			if (completeCheckBox.checked == true) {
-				console.log("index : item checked as completed");
-			} else {
-				console.log("index : item not checked as completed");
-			}
-		});
-	}
+ 		completeCheckBox.addEventListener("change", (e) => {
+ 			if (completeCheckBox.checked == true) {
+  				console.log("index : item checked as completed");
+ 			} else {
+ 				console.log("index : item not checked as completed");
+ 			}
+ 		});
+ 	}
 };
 
 const deleteTasks = () => {};
 
 submitButton.addEventListener("click", addItemToList);
 
-// grab buttons
-const completeButton = document.querySelector("#complete");
-const deleteButton = document.querySelector("#delete");
-// console.log(checkbox);
+// Miah's old code
 
-// grab section
-const completedSection = document.querySelector("#completed");
+// // grab buttons
+// const completeButton = document.querySelector("#complete");
+// const deleteButton = document.querySelector("#delete");
+// // console.log(checkbox);
 
-completeButton.addEventListener("click", event => {
-  // get all heckboxes in todolist section
-  const checkbox = document.querySelectorAll('#todoList input[type="checkbox"]');
-  // console.log(checkbox);
-  // loop over to find checked checkbox
-  for (let i = 0; i < checkbox.length; i++) {
-    // console.log(i);
-    if (checkbox[i].checked) {
-      // target li which have data value = to checkbox ticked
-      const listItem = document.querySelector(`li[data-value=${checkbox[i].value}]`);
-      // append to new section
-      completedSection.appendChild(listItem);
-    }
-  }
-});
+// // grab section
+// const completedSection = document.querySelector("#completed");
 
-deleteButton.addEventListener("click", event => {
-  const checkbox = document.querySelectorAll('#completed input[type="checkbox"]');
-  // console.log(checkbox);
-  for (let i = 0; i < checkbox.length; i++) {
-    if (checkbox[i].checked) {
-      const listItem = document.querySelector(`li[data-value=${checkbox[i].value}]`);
-      listItem.remove();
-    }
-  }
-});
+// completeButton.addEventListener("click", event => {
+//   // get all heckboxes in todolist section
+//   const checkbox = document.querySelectorAll('#todoList input[type="checkbox"]');
+//   // console.log(checkbox);
+//   // loop over to find checked checkbox
+//   for (let i = 0; i < checkbox.length; i++) {
+//     // console.log(i);
+//     if (checkbox[i].checked) {
+//       // target li which have data value = to checkbox ticked
+//       const listItem = document.querySelector(`li[data-value=${checkbox[i].value}]`);
+//       // append to new section
+//       completedSection.appendChild(listItem);
+//     }
+//   }
+// });
+
+// deleteButton.addEventListener("click", event => {
+//   const checkbox = document.querySelectorAll('#completed input[type="checkbox"]');
+//   // console.log(checkbox);
+//   for (let i = 0; i < checkbox.length; i++) {
+//     if (checkbox[i].checked) {
+//       const listItem = document.querySelector(`li[data-value=${checkbox[i].value}]`);
+//       listItem.remove();
+//     }
+//   }
+// });
